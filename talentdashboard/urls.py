d58c2ed44f69ed82b56735c91d87278194aa6556
Mark Kennedy <mark.kennedy@gmail.com>
2013-09-26 13:20:46 -0400

    Changed url to /employees/:id

--- talentdashboard/urls.py
+++ talentdashboard/urls.py
@@ -19,7 +19,7 @@ v1_api.register(EvaluationRoundResource())
 
 urlpatterns = patterns('',
     url(r'^$',  TemplateView.as_view(template_name="index.html"), name='home'),
-    url(r'^employee$',  TemplateView.as_view(template_name="employee.html"), name='employee'),
+    url(r'^employees/(?P<id>\d+)$',  TemplateView.as_view(template_name="employee.html"), name='employee'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_api.urls)),
 )
