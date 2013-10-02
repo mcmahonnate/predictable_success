170442f06d817b19158fefec123ab68e9238e304
Mark Kennedy <mark.kennedy@gmail.com>
2013-10-01 21:46:18 -0400

    Added categorization of PVP evaluations

--- pvp/models.py
+++ pvp/models.py
@@ -14,6 +14,24 @@ class PvpEvaluation(models.Model):
     potential = models.IntegerField(choices=PVP_SCALE)
     performance = models.IntegerField(choices=PVP_SCALE)
 
+    def is_top_performer(self):
+        return self.potential == self.performance == 4
+
+    def is_strong_performer(self):
+        return (self.potential == 4 and self.performance == 3) or (self.potential == 3 and self.performance == 4)
+
+    def is_good_performer(self):
+        return self.potential == self.performance == 3
+
+    def is_in_wrong_role(self):
+        return self.potential >=3 and self.performance < 3
+
+    def lacks_potential(self):
+        return self.potential < 3 and self.performance >= 3
+
+    def needs_drastic_change(self):
+        return self.potential <=2 and self.performance <= 2
+
     def __str__(self):
         return "%s PVP Evaluation %s" % (self.employee.informal_name, self.evaluation_round.date)
 
--- pvp/tests.py
+++ pvp/tests.py
@@ -8,6 +8,100 @@ evaluation_round = EvaluationRound(date = datetime.date(2012, 12, 31))
 
 class PvpEvaluationTest(TestCase):
 
+    def create_pvp(self, potential=0, performance=0):
+        pvp = PvpEvaluation(employee=employee, evaluation_round = evaluation_round)
+        pvp.potential = potential
+        pvp.performance = performance
+        return pvp
+
+    def test_is_top_performer(self):
+        pvp = self.create_pvp(potential=4, performance=4)
+        result = pvp.is_top_performer()
+        self.assertTrue(result)
+        result = pvp.is_strong_performer() \
+            and pvp.is_good_performer() \
+            and pvp.is_in_wrong_role \
+            and pvp.lacks_potential() \
+            and pvp.needs_drastic_change
+        self.assertFalse(result)
+
+    def test_is_strong_performer(self):
+        self.assert_is_strong_performer(potential=4, performance=3)
+        self.assert_is_strong_performer(potential=3, performance=4)
+
+    def assert_is_strong_performer(self, potential, performance):
+        pvp = self.create_pvp(potential=potential, performance=performance)
+        result = pvp.is_strong_performer()
+        self.assertTrue(result)
+        result = pvp.is_top_performer() \
+            and pvp.is_good_performer() \
+            and pvp.is_in_wrong_role \
+            and pvp.lacks_potential() \
+            and pvp.needs_drastic_change
+        self.assertFalse(result)
+
+    def test_is_good_performer(self):
+        pvp = self.create_pvp(potential=3, performance=3)
+        result = pvp.is_good_performer()
+        self.assertTrue(result)
+        result = pvp.is_top_performer() \
+            and pvp.is_strong_performer() \
+            and pvp.is_in_wrong_role \
+            and pvp.lacks_potential() \
+            and pvp.needs_drastic_change
+        self.assertFalse(result)
+
+    def test_is_in_wrong_role(self):
+        self.assert_is_in_wrong_role(potential=3, performance=2)
+        self.assert_is_in_wrong_role(potential=3, performance=1)
+        self.assert_is_in_wrong_role(potential=4, performance=2)
+        self.assert_is_in_wrong_role(potential=4, performance=1)
+
+    def assert_is_in_wrong_role(self, potential, performance):
+        pvp = self.create_pvp(potential=potential, performance=performance)
+        result = pvp.is_in_wrong_role()
+        self.assertTrue(result)
+        result = pvp.is_top_performer() \
+            and pvp.is_strong_performer() \
+            and pvp.is_good_performer \
+            and pvp.lacks_potential() \
+            and pvp.needs_drastic_change
+        self.assertFalse(result)
+
+    def test_lacks_potential(self):
+        self.assert_lacks_potential_is_true(potential=2, performance=3)
+        self.assert_lacks_potential_is_true(potential=1, performance=3)
+        self.assert_lacks_potential_is_true(potential=2, performance=4)
+        self.assert_lacks_potential_is_true(potential=1, performance=4)
+
+    def assert_lacks_potential_is_true(self, potential, performance):
+        pvp = self.create_pvp(potential=potential, performance=performance)
+        result = pvp.lacks_potential()
+        self.assertTrue(result)
+        result = pvp.is_top_performer() \
+            and pvp.is_strong_performer() \
+            and pvp.is_good_performer \
+            and pvp.is_in_wrong_role() \
+            and pvp.needs_drastic_change
+        self.assertFalse(result)
+
+    def test_needs_drastic_change(self):
+        self.assert_needs_drastic_change(potential=2, performance=2)
+        self.assert_needs_drastic_change(potential=2, performance=1)
+        self.assert_needs_drastic_change(potential=1, performance=2)
+        self.assert_needs_drastic_change(potential=1, performance=1)
+
+    def assert_needs_drastic_change(self, potential, performance):
+        pvp = self.create_pvp(potential=potential, performance=performance)
+        result = pvp.needs_drastic_change()
+        self.assertTrue(result)
+        result = pvp.is_top_performer() \
+            and pvp.is_strong_performer() \
+            and pvp.is_good_performer \
+            and pvp.is_in_wrong_role() \
+            and pvp.lacks_potential()
+        self.assertFalse(result)
+
     def test_str(self):
         pvp = PvpEvaluation(employee=employee, evaluation_round = evaluation_round)
         expected = "John Doe PVP Evaluation 2012-12-31"
