55e6751dbba1d583cb48230f536996c2e025031b
Mark Kennedy <mark.kennedy@gmail.com>
2013-10-23 13:50:07 -0400

    Changed API authentication and permissions; you now must be logged in (but not necessarily be an admin)

--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -118,7 +118,12 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
