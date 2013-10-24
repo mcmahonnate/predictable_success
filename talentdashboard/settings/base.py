57d87392258cbc7e9a38a2d9fddfaf3b1273a295
Mark Kennedy <mark.kennedy@gmail.com>
2013-10-24 15:06:37 -0400

    nurd

--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -47,11 +47,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__), 'staticfiles'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
