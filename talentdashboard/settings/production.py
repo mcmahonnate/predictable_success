b86515f58e16ceca376400c6aaaf36a0c493d7fa
Mark Kennedy <mark.kennedy@gmail.com>
2013-10-24 13:12:21 -0400

    Debugging deployment

--- talentdashboard/settings/production.py
+++ talentdashboard/settings/production.py
@@ -2,9 +2,8 @@
 from .base import *
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+raise Exception(DATABASE_URL)
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
