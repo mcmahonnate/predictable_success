63d5a21a0a47151c54f9f6e3df700c12c22ca907
Mark Kennedy <mark.kennedy@gmail.com>
2013-10-24 14:17:57 -0400

    bork bork

--- talentdashboard/settings/production.py
+++ talentdashboard/settings/production.py
@@ -2,7 +2,7 @@
 from .base import *
 import dj_database_url
 
-DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
