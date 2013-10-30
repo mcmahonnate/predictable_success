3426aa968ab5cfbd94620c5d7552663dce6d0b2c
mcmahonnate <mcmahon.nate@gmail.com>
2013-10-30 13:33:17 -0400

    Changing Media root to be environment specific

--- talentdashboard/settings/production.py
+++ talentdashboard/settings/production.py
@@ -2,6 +2,15 @@
 from .base import *
 import dj_database_url
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = '/talentdashboard/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+
 DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
