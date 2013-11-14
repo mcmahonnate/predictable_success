0685ffab09b60a70a8e0b2b1558da2d29d6bdb67
mcmahonnate <mcmahon.nate@gmail.com>
2013-11-14 06:42:34 -0500

    Applying session time out

--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -87,9 +87,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'talentdashboard.middleware.AutoLogout',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+AutoLogout=1
 
 ROOT_URLCONF = 'talentdashboard.urls'
 
