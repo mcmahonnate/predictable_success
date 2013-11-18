4b866d86d637f8ed075ff48c32b6524bca5e5acf
mcmahonnate <mcmahon.nate@gmail.com>
2013-11-18 14:00:27 -0500

    Gah!

--- org/models.py
+++ org/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import blah
 
 class Employee(models.Model):
@@ -32,6 +33,7 @@ class Employee(models.Model):
         blank=True,
         default=None
     )
+    user = models.OneToOneField(User,on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.full_name
