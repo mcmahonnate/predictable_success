381d106cb646f82893379e75f0e0fc6a50655f70
mcmahonnate <mcmahon.nate@gmail.com>
2014-01-13 07:30:18 -0500

    cleaning up discussions partial

--- talentdashboard/static/angular/app.js
+++ talentdashboard/static/angular/app.js
@@ -6,7 +6,8 @@ var app = angular.module('tdb', ['tdb.services', 'tdb.controllers', 'tdb.directi
           when('/teams/:id/', {templateUrl: '/static/angular/partials/team-overview.html', controller: 'TeamOverviewCtrl', resolve: {authorizeRoute: authorizeRoute}}).
           when('/employees', {templateUrl: '/static/angular/partials/employee-list.html', controller: 'EmployeeListCtrl', resolve: {authorizeRoute: authorizeRoute}}).
           when('/employees/:id', {templateUrl: '/static/angular/partials/employee-detail.html', controller: 'EmployeeDetailCtrl', resolve: {authorizeRoute: authorizeRoute}}).
-          when('/discussions/:id', {templateUrl: '/static/angular/partials/discussions-overview.html', controller: 'EmployeeDetailCtrl', resolve: {authorizeRoute: authorizeRoute}}).
+          when('/discussion', {templateUrl: '/static/angular/partials/discussion-overview.html', controller: 'DiscussionOverviewCtrl', resolve: {authorizeRoute: authorizeRoute}}).
+          when('/discussion/:id', {templateUrl: '/static/angular/partials/discussion-detail.html', controller: 'EmployeeDetailCtrl', resolve: {authorizeRoute: authorizeRoute}}).
           when('/leaders/:id', {templateUrl: '/static/angular/partials/leader-detail.html', controller: 'LeaderDetailCtrl', resolve: {authorizeRoute: authorizeRoute}}).          
           otherwise({redirectTo: '/'});
     }]);
