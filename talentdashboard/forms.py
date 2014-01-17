357d897376c0e519b6fbef3060cee197062ab812
mcmahonnate <mcmahon.nate@gmail.com>
2014-01-17 06:55:22 -0500

    Adding some cosmetics to auth experience

--- talentdashboard/forms.py
+++ talentdashboard/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, SetPasswordForm
 from django.forms.widgets import PasswordInput, TextInput
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput(attrs={'placeholder': 'Username or Email', 'autofocus':''}))
     password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
+
+class CustomSetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'New password', 'autofocus':''}))
+    new_password2 = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Re-enter new password'}))
