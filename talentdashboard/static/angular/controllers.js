1fe4cc78e3641b9554ddc9c4c552210fc66c1dad
mcmahonnate <mcmahon.nate@gmail.com>
2014-01-24 16:04:14 -0500

    Adding a pop up calendar control

--- talentdashboard/static/angular/controllers.js
+++ talentdashboard/static/angular/controllers.js
@@ -417,6 +417,54 @@ angular.module('tdb.controllers', [])
     }
 }])
 
+.controller('DatePickerCtrl', ['$scope', '$routeParams', 'EmployeeToDo','ToDo', function($scope, $routeParams, EmployeeToDo,ToDo) {
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.showWeeks = false;
+    $scope.toggleWeeks = function () {
+        $scope.showWeeks = ! $scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = ( $scope.minDate ) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.close = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = false;
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yy'",
+        'starting-day': 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+    $scope.format = $scope.formats[0];
+}])
+
 .controller('EmployeeCommentsCtrl', ['$scope', '$filter', '$routeParams', '$window', 'EmployeeComments', 'SubComments','Comment', 'User', function($scope, $filter, $routeParams, $window, EmployeeComments, SubComments, Comment, User) {
     $scope.employeeId = $routeParams.id;
     $scope.newCommentText = "";
