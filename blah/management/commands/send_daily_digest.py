7c2560f09439e8d587829d4331eee70210df425f
mcmahonnate <mcmahon.nate@gmail.com>
2014-01-28 15:28:33 -0500

    Daily Digest Email polish

--- blah/management/commands/send_daily_digest.py
+++ blah/management/commands/send_daily_digest.py
@@ -3,6 +3,10 @@ from django.core.mail import EmailMultiAlternatives
 from datetime import datetime, timedelta
 from django.utils.dateformat import DateFormat
 from django.contrib.auth.models import User
+from django.template.loader import get_template
+from django.template import Context
+from django.contrib.sites.models import get_current_site
+from django.contrib.contenttypes.models import ContentType
 from blah.models import Comment
 from todo.models import Task
 
@@ -12,17 +16,10 @@ class Command(BaseCommand):
         dt = datetime.now()
         start_dt = dt-timedelta(days=7)
         comments = Comment.objects.filter(created_date__range=[start_dt,dt])
-        text_content = None
-        html_content = None
-        for comment in comments:
-            self.stdout.write(comment.content)
-            if text_content is not None:
-                text_content = text_content + comment.content
-                html_content = html_content + "<p>" + comment.content + "</p>"
-            else:
-                text_content = comment.content
-                html_content = comment.content
-        if text_content is not None:
+        todos = Task.objects.filter(created_date__range=[start_dt,dt])
+        plaintext = get_template('daily_digest_email.txt')
+        htmly = get_template('daily_digest_email.html')
+        if comments.count > 0 or todos.count > 0:
             recipients = User.objects.filter(groups__id=3)
             recipient_list = None
             if recipients is not None:
@@ -34,6 +31,12 @@ class Command(BaseCommand):
                 df = DateFormat(dt)
                 from_email = 'People First<peoplefirst@dfrntlabs.com>'
                 subject = 'Daily Recap for ' +  df.format('l, d F')
+                site = get_current_site(None).domain
+                date = df.format('l, d F')
+                comment_type = ContentType.objects.get(model="comment")
+                data = Context({ 'date': date, 'comments': comments, 'todos': todos, 'site': site, 'comment_type': comment_type })
+                text_content = plaintext.render(data)
+                html_content = htmly.render(data)
                 msg = EmailMultiAlternatives(subject, text_content, from_email, [recipient_list])
                 msg.attach_alternative(html_content, "text/html")
                 msg.send()
--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -81,6 +81,10 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_DIRS = (
+    "/django-talentdashboard/talentdashboard/templates",
+)
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
 )
