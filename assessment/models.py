7dbe868b6d46edb69cbd393c62214f72a0679754
mcmahonnate <mcmahon.nate@gmail.com>
2014-09-10 20:39:24 -0400

    Refactoring attributes to a new model

---
+++ assessment/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from org.models import Employee
+
+class AssessmentType(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+class AssessmentCategory(models.Model):
+    assessment = models.ForeignKey(AssessmentType, related_name='+')
+    name = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return "%s - %s" % (self.assessment.name, self.name)
+
+class AssessmentBand(models.Model):
+    category = models.ForeignKey(AssessmentCategory, related_name='+')
+    min_value = models.IntegerField()
+    max_value = models.IntegerField()
+    name = models.CharField(
+        max_length=255,
+    )
+    description = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+class EmployeeAssessment(models.Model):
+    employee = models.ForeignKey(Employee, related_name='+')
+    category = models.ForeignKey(AssessmentCategory, related_name='+')
+    score = models.IntegerField()
+
+    def __str__(self):
+        return "%s" % (self.employee.name)
+
+class AssessmentComparison(models.Model):
+    this = models.ForeignKey(AssessmentBand, related_name='+')
+    that = models.ForeignKey(AssessmentBand, related_name='+')
+    assessed_date = models.DateField(auto_now_add = True)
+    description = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return "%s compared to %s" % (self.this.name, self.that.name)
