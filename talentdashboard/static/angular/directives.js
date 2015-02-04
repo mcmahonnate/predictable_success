277a16e0dd252d74fa49a66fa9155e4cde46776a
mcmahonnate <mcmahon.nate@gmail.com>
2015-02-04 11:50:28 -0500

    roughing out edit employee modal dialog

--- talentdashboard/static/angular/directives.js
+++ talentdashboard/static/angular/directives.js
@@ -496,6 +496,28 @@ angular.module('tdb.directives', [])
   }
 })
 
+.directive('modalEditEmployee', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    templateUrl: "/static/angular/partials/modal-edit-employee.html"
+  };
+})
+
 .directive('sliderQuickStart', function() {
   return {
     link: function(scope, elem, attrs) {
