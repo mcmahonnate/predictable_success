e68dc4f36ebe08cbd96ae8c46eb6e9aadda55238
mcmahonnate <mcmahon.nate@gmail.com>
2015-02-17 08:50:03 -0500

    Adding validation to add employee

--- talentdashboard/static/angular/controllers.js
+++ talentdashboard/static/angular/controllers.js
@@ -332,7 +332,7 @@ angular.module('tdb.controllers', [])
 	}
 }])
 
-.controller('EmployeeDetailCtrl', ['$rootScope', '$scope', '$location', '$routeParams', '$window', 'User', 'Employee', 'Engagement', 'EmployeeLeader', 'Attribute', 'CompSummary', 'PhotoUpload', '$http', 'SitePreferences', 'analytics', 'fileReader','Assessment','EmployeeMBTI', function($rootScope, $scope, $location, $routeParams, $window, User, Employee, Engagement, EmployeeLeader, Attribute, CompSummary, PhotoUpload, $http, SitePreferences, analytics, fileReader, Assessment, EmployeeMBTI) {
+.controller('EmployeeDetailCtrl', ['$rootScope', '$scope', '$location', '$routeParams', '$window', 'User', 'Employee', 'Engagement', 'EmployeeLeader', 'Attribute', 'CompSummary', '$http', 'SitePreferences', 'analytics', 'fileReader','Assessment','EmployeeMBTI', function($rootScope, $scope, $location, $routeParams, $window, User, Employee, Engagement, EmployeeLeader, Attribute, CompSummary, $http, SitePreferences, analytics, fileReader, Assessment, EmployeeMBTI) {
     analytics.trackPage($scope, $location.absUrl(), $location.url());
     SitePreferences.get(function (data) {
         $scope.site_preferences = data;
