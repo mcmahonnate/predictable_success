a9a5c9cbcc262a4ef0df49589531882973945787
Mark Kennedy <mark.kennedy@gmail.com>
2015-04-08 13:37:36 -0400

    Changing URL for test environment

--- talentdashboard/settings/production.py
+++ talentdashboard/settings/production.py
@@ -2,7 +2,7 @@
 from .base import *
 import dj_database_url
 
-DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
+DATABASES = { 'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRESQL_ORANGE_URL')) }
 DEBUG = os.environ.get("DEBUG", False)
 EMAIL_BACKEND = 'talentdashboard.backend.SSLEmailBackend'
 EMAIL_USE_SSL = True
