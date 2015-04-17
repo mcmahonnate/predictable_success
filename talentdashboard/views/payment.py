63596099a463963d4b1a3c9dfb863a2b78e3c849
mcmahonnate <mcmahon.nate@gmail.com>
2015-04-17 15:03:17 -0400

    passing correct keys to stripe

--- talentdashboard/views/payment.py
+++ talentdashboard/views/payment.py
@@ -26,7 +26,12 @@ class ChargeView(TemplateView):
         #return render_to_response(self.success_url, {}, context_instance=RequestContext(request))
 
     def post(self, request, *args, **kwargs):
-        stripe.api_key = settings.STRIPE_KEY
+        stripe_keys = {
+            'secret_key': settings.STRIPE_SECRET_KEY,
+            'publishable_key': settings.STRIPE_PUBLISHABLE_KEY
+        }
+
+        stripe.api_key = stripe_keys['secret_key']
         token = request.POST.get('stripeToken', '')
         email = request.POST.get('stripeEmail', '')
         employees = request.POST.get('employees', '')
