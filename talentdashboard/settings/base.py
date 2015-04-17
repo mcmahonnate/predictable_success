20216e25745dfe86cf5b5cd5d1f8478eedba7b0d
Doug Dosberg <ddosberg@fool.com>
2015-04-17 11:33:14 -0400

    fixing the settings file

--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -73,8 +73,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = '/static/'
-COMPRESS_URL = '/staticfiles/'
+COMPRESS_URL = STATIC_URL
 
 
 # Additional locations of static files
