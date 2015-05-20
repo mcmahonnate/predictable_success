322a025ceec204de154880e9722ef43a3b8bf996
Mark Kennedy <mark.kennedy@gmail.com>
2015-05-20 13:48:47 -0400

    Put request transformation in service

--- talentdashboard/static/angular/services/tasks.js
+++ talentdashboard/static/angular/services/tasks.js
@@ -1,15 +1,35 @@
 angular.module('tdb.services.tasks', ['ngResource'])
 
-.factory('MyTasks', ['$resource', function($resource) {
-    return $resource('/api/v1/tasks/mine/');
-}])
+    .factory('MyTasks', ['$resource', function ($resource) {
+        return $resource('/api/v1/tasks/mine/');
+    }])
 
-.factory('Task', ['$resource', function($resource) {
-    var extraMethods = {
-        'update': {
-            method: 'PUT'
-        }
-    };
-    return $resource('/api/v1/tasks/:id/', {id:'@id'}, extraMethods);
-}])
+    .factory('Task', ['$resource', function ($resource) {
+        var forEditing = function (task) {
+            var copy = angular.copy(task);
+            return {
+                    id: copy.id,
+                    employee: copy.employee.id,
+                    assigned_to: copy.assigned_to ? copy.assigned_to.id : null,
+                    assigned_by: copy.assigned_by ? copy.assigned_by.id : null,
+                    due_date: copy.due_date,
+                    completed: copy.completed,
+                    description: copy.description
+                };
+        };
+
+        var extraMethods = {
+            'update': {
+                method: 'PUT',
+                transformRequest: [
+                    forEditing,
+                    angular.toJson
+                ]
+            }
+        };
+
+        var Task = $resource('/api/v1/tasks/:id/', {id: '@id'}, extraMethods);
+
+        return Task;
+    }])
 ;
