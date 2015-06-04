082b78a2236125f225f16d2c60bfdfc721f2b2ee
mcmahonnate <mcmahon.nate@gmail.com>
2015-06-04 13:59:58 -0400

    Including survey email preferences in Customer serializer

--- talentdashboard/views/serializers.py
+++ talentdashboard/views/serializers.py
@@ -332,7 +332,7 @@ class KPIPerformanceSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('id', 'name', 'domain_url', 'show_kolbe', 'show_vops', 'show_mbti', 'show_coaches', 'show_timeline', 'show_kpi')
+        fields = ('id', 'name', 'domain_url', 'show_kolbe', 'show_vops', 'show_mbti', 'show_coaches', 'show_timeline', 'show_kpi', 'survey_email_subject', 'survey_email_body')
 
 
 class SubCommentSerializer(serializers.HyperlinkedModelSerializer):
