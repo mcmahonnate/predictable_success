8422578ed00f36f84a21b6572d6c6d3042bfa474
Mark Kennedy <mark.kennedy@gmail.com>
2015-06-09 15:07:09 -0400

    Adding config for installing Solr

--- install_solr.sh
+++ install_solr.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+echo "Downloading Solr 4.4.0..."
 curl http://archive.apache.org/dist/lucene/solr/4.4.0/solr-4.4.0.tgz | tar xz -C ~/
-cp -rf ~/solr-4.4.0/example/solr/collection1/ ~/solr-4.4.0/example/solr/employees
+echo "Creating Employees core..."
+unzip employees.zip -d ~/solr-4.4.0/example/solr/
+echo "Installing Employees schema..."
 cp solr-employees-schema.xml ~/solr-4.4.0/example/solr/employees/schema.xml
-java -jar ~/solr-4.4.0/example/start.jar
+echo "Done. You can now start Solr with the following:"
+echo "cd ~/solr-4.4.0/example/"
+echo "java -jar start.jar"
 
 
