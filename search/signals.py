056398a1a5801f86be9c938054c440b24e436fdb
Mark Kennedy <mark.kennedy@gmail.com>
2015-06-10 05:42:59 -0400

    Reindex employee when a PvpEvaluation is saved or deleted

--- search/signals.py
+++ search/signals.py
@@ -2,32 +2,51 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from org.models import Employee, Leadership
 from customers.models import Customer, current_customer
+from pvp.models import PvpEvaluation
 from indexes import EmployeeIndex
+
 
 @receiver(post_save, sender=Employee)
 def employee_save_handler(sender, **kwargs):
-    index = EmployeeIndex()
     employee = kwargs['instance']
-    try:
-        index.process([employee], current_customer())
-    except Customer.DoesNotExist:
-        pass
+    _index([employee])
 
 @receiver(post_delete, sender=Employee)
 def employee_delete_handler(sender, **kwargs):
-    index = EmployeeIndex()
     employee = kwargs['instance']
-    try:
-        index.delete(employee, current_customer())
-    except Customer.DoesNotExist:
-        pass
+    _delete(employee)
+
 
 @receiver(post_save, sender=Leadership)
 def leadership_save_handler(sender, **kwargs):
-    index = EmployeeIndex()
     leadership = kwargs['instance']
+    employees = [leadership.leader, leadership.employee]
+    _index(employees)
+
+
+@receiver(post_save, sender=PvpEvaluation)
+def pvp_evaluation_save_handler(sender, **kwargs):
+    pvp = kwargs['instance']
+    _index([pvp.employee])
+
+
+@receiver(post_delete, sender=PvpEvaluation)
+def pvp_evaluation_delete_handler(sender, **kwargs):
+    pvp = kwargs['instance']
+    _index([pvp.employee])
+
+
+def _index(employees):
+    index = EmployeeIndex()
     try:
-        employees = [leadership.leader, leadership.employee]
         index.process(employees, current_customer())
+    except Customer.DoesNotExist:
+        pass
+
+
+def _delete(employee):
+    index = EmployeeIndex()
+    try:
+        index.delete(employee, current_customer())
     except Customer.DoesNotExist:
         pass
