7b2ad5ecc9c2964d99c9c9c441ed2afd29d5dcdc
Mark Kennedy <mark.kennedy@gmail.com>
2015-06-30 15:26:51 -0400

    Added checkin type

--- checkins/models.py
+++ checkins/models.py
@@ -3,9 +3,21 @@ from org.models import Employee
 from engagement.models import Happiness
 
 
+class CheckInType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    sort_weight = models.IntegerField()
+
+    class Meta:
+        ordering = ['sort_weight']
+
+
 class CheckIn(models.Model):
     employee = models.ForeignKey(Employee, related_name='checkins', null=False, blank=False)
     host = models.ForeignKey(Employee, related_name='checkins_hosted', null=False, blank=False)
     date = models.DateTimeField(null=False, blank=False)
     summary = models.TextField(null=False, blank=False)
+    type = models.ForeignKey(CheckInType, related_name='+', null=True)
+    other_type_description = models.CharField(max_length=100, null=True, blank=True)
     happiness = models.ForeignKey(Happiness, null=True, blank=True)
+
+
