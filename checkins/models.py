0404b868c4a228d674aa126d71adebf340b94387
Mark Kennedy <mark.kennedy@gmail.com>
2015-07-07 10:12:03 -0400

    Added __unicode__ methods to models

--- checkins/models.py
+++ checkins/models.py
@@ -7,6 +7,9 @@ class CheckInType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     sort_weight = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         ordering = ['sort_weight']
 
@@ -20,4 +23,10 @@ class CheckIn(models.Model):
     other_type_description = models.CharField(max_length=100, null=True, blank=True)
     happiness = models.ForeignKey(Happiness, null=True, blank=True)
 
+    def get_type_description(self):
+        if self.type is not None:
+            return self.type.name
+        return self.other_type_description
 
+    def __unicode__(self):
+        return u'{0} "{1}" Check-in with {2} on {3}'.format(self.host, self.get_type_description(), self.employee, self.date.strftime('%x'))
