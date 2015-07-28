80da26d88f0a5364e93cd1408c30b00b4f12f320
Doug Dosberg <ddosberg@fool.com>
2015-07-28 15:22:10 -0400

    Fixed unescaped semi-quotes in scoutmap.sql... potentially fixing the duplicate key value error we've been seeing

---
+++ activity/migrations/0008_auto_20150727_1508.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('activity', '0007_auto_20150722_0033'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 7, 27, 15, 8, 5, 22729)),
+            preserve_default=True,
+        ),
+    ]
