02c495b2ed781601868fe10c8fed5b9adc46c017
Doug Dosberg <ddosberg@fool.com>
2015-09-02 16:10:24 -0400

    Tweeks to controller

--- talentdashboard/static/angular/controllers/comments.js
+++ talentdashboard/static/angular/controllers/comments.js
@@ -2,18 +2,14 @@ angular.module('tdb.controllers.comments', [])
 
     .controller('CheckinCommentCtrl', ['$scope', '$rootScope', '$routeParams', 'Comment', function($scope, $rootScope, $routeParams, Comment) {
         
-        $scope.newComment = new Comment();
+        $scope.newComment = new Comment({content:'', include_in_daily_digest:true});
 
         $scope.add = function(form) {
 
-            console.log(form);
-
             if (form.$invalid) return;
-            Comment.addToCheckIn({ id:$routeParams.id}, $scope.newComment)
-                .$promise.then(function(comment) {
-                    $scope.newComment = new Comment();
-
-                    $rootScope.$broadcast("commentCreated", comment);
-                });
+            Comment.addToCheckIn({ id:$routeParams.id}, $scope.newComment, function(comment) {
+                $scope.newComment = new Comment({content:'', include_in_daily_digest:true});
+                $rootScope.$broadcast("commentCreated", comment);
+            });
         }      
     }]);
