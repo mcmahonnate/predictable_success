5c1c11f3980932b4784cb15357a03b01ed294b18
mcmahonnate <mcmahon.nate@gmail.com>
2015-09-08 20:26:09 -0400

    Fixing route to login page

--- talentdashboard/static/angular/apps/app-base.js
+++ talentdashboard/static/angular/apps/app-base.js
@@ -10,18 +10,21 @@ var app = angular.module('tdb', [
         'tdb.services.activity', 'tdb.controllers.activity',
         'tdb.checkins.services', 'tdb.checkins.controllers',
         'tdb.engagement.services', 'angular.filter',
-        'angular-carousel', 'analytics', 'ui.bootstrap', 'ngCsv','ngImgCrop', 'ngRoute','ui-notification', 'ngMessages'
-]);
-
-app.run(['$rootScope', 'User', 'TalentCategories', 'Customers', function($rootScope, User, TalentCategories, Customers) {
-    User.get(function(data) {
-        $rootScope.currentUser = data;
-    });
-    Customers.get(function(data) {
-        $rootScope.customer = data;
-    });
-    $rootScope.talentCategories = TalentCategories.categories;
-}]);
+        'angular-carousel', 'analytics', 'ui.bootstrap', 'ngCsv','ngImgCrop', 'ngRoute','ui-notification', 'ngMessages'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/', {resolve: {authorizeRoute: authorizeRoute}}).
+            otherwise({redirectTo: '/'});
+    }])
+    .run(['$rootScope', 'User', 'TalentCategories', 'Customers', function($rootScope, User, TalentCategories, Customers) {
+        User.get(function(data) {
+            $rootScope.currentUser = data;
+        });
+        Customers.get(function(data) {
+            $rootScope.customer = data;
+        });
+        $rootScope.talentCategories = TalentCategories.categories;
+    }]);
 
 app.config(function($resourceProvider) {
   $resourceProvider.defaults.stripTrailingSlashes = false;
