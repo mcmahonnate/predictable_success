393ed4a159f64f421b17df928d68127f95c70719
Mark Kennedy <mark.kennedy@gmail.com>
2015-10-06 10:38:58 -0400

    Added gulp, though not using it yet

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+    return gulp.src('./talentdashboard/static/angular/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./talentdashboard/staticfiles/'));
+});
+
+gulp.task('less', function () {
+    return gulp.src('./talentdashboard/static/css/scoutmap/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./talentdashboard/staticfiles/CACHE/css/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./talentdashboard/**/*.less', ['less']);
+    gulp.watch('./talentdashboard/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['watch']);
