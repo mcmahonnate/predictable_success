f9cee57aceea8310b2c72af129457c0c8ab59b2a
mcmahonnate <mcmahon.nate@gmail.com>
2015-10-06 16:32:02 -0400

    Adding Pop up to Engagement Survey

--- talentdashboard/static/angular/engagement/controllers.js
+++ talentdashboard/static/angular/engagement/controllers.js
@@ -1,6 +1,6 @@
 angular.module('tdb.engagement.controllers', [])
     
-    .controller('EngagementSurveyCtrl', ['$scope', '$window', '$routeParams', '$location', 'EngagementSurvey', 'analytics', function ($scope, $window, $routeParams, $location, EngagementSurvey, analytics) {
+    .controller('EngagementSurveyCtrl', ['$scope', '$window', '$routeParams', '$location', '$modal', 'EngagementSurvey', 'analytics', function ($scope, $window, $routeParams, $location, $modal, EngagementSurvey, analytics) {
         analytics.trackPage($scope, $location.absUrl(), $location.url());
         $scope.employee_id = $routeParams.employeeId;
         $scope.survey_id = $routeParams.surveyId;
@@ -8,6 +8,9 @@ angular.module('tdb.engagement.controllers', [])
         $scope.error = false;
         EngagementSurvey.getSurvey($scope.employee_id, $scope.survey_id).$promise.then(function (response) {
                 $scope.survey = response;
+                if ($scope.survey.active && !$scope.survey.complete){
+                    showWhoCanSeeThis();
+                }
             }, function (response) {
                 $scope.error = true
             }
@@ -15,6 +18,16 @@ angular.module('tdb.engagement.controllers', [])
         $scope.happy = {assessment: 0};
         $scope.happy.comment = {visibility: 3, content: ''};
 
+        showWhoCanSeeThis = function () {
+            $modal.open({
+                animation: true,
+                templateUrl: '/static/angular/partials/_modals/who-can-see-this.html',
+                controller: 'DailyDigestCtrl'
+            });
+        }
+        $scope.cancel = function () {
+            $modalInstance.dismiss();
+        };
         $scope.save_engagement = function () {
             var data = {id: $scope.employee_id, survey_id: $scope.survey_id, _assessment: $scope.happy.assessment, _content: $scope.happy.comment.content};
             EngagementSurvey.save(data, function (response) {
