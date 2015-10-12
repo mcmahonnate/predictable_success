eca841e6db7b16192486dd36580f7e924677182e
Mark Kennedy <mark.kennedy@gmail.com>
2015-10-12 13:18:59 -0400

    Added 'sm' prefix to feedback submission form directive

--- talentdashboard/static/angular/feedback/feedback-submission-form.directive.js
+++ talentdashboard/static/angular/feedback/feedback-submission-form.directive.js
@@ -1,7 +1,7 @@
 (function() {
     angular
         .module('feedback')
-        .directive('feedbackSubmissionForm', feedbackSubmissionForm);
+        .directive('smFeedbackSubmissionForm', feedbackSubmissionForm);
 
     function feedbackSubmissionForm() {
         return {
