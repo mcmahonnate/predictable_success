3243e32781af4bf32de3cd703ebb4746e130edf1
mcmahonnate <mcmahon.nate@gmail.com>
2015-11-24 14:12:11 -0500

    Fixed: Add focus areas to Feedback Submission

--- talentdashboard/static/angular/feedback/respond-to-feedback-request.controller.js
+++ talentdashboard/static/angular/feedback/respond-to-feedback-request.controller.js
@@ -7,12 +7,23 @@
         BaseSubmitFeedbackController.call(this, $location, $modal, $rootScope);
         var vm = this;
         vm.feedbackRequest = null;
+        vm.goTo = goTo;
 
         activate();
 
         function activate() {
             getFeedbackRequest();
         }
+
+        function goTo(path) {
+            var cancel = confirm("Are you sure you want to lose all the great feedback you've already written?");
+            if (cancel == true) {
+                $location.path(path);
+                $('.modal').modal('hide');
+                $('.modal-backdrop').remove();
+                $('body').removeClass('modal-open');
+            }
+        };
 
         function getFeedbackRequest() {
             return FeedbackRequestService.getFeedbackRequest($routeParams.id)
