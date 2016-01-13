111fa722143c680709e6b4f406ff59be196da456
mcmahonnate <mcmahon.nate@gmail.com>
2016-01-13 15:52:21 -0500

    Add notification when someone has commented on a checkin

--- blah/signals.py
+++ blah/signals.py
@@ -2,12 +2,16 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Comment
-from .tasks import send_comment_reply_notification
+from checkins.models import CheckIn
+from .tasks import send_comment_reply_notification, send_checkin_reply_notification
 
 
 @receiver(post_save, sender=Comment)
 def comment_save_handler(sender, instance, created, **kwargs):
     if created:
-        comment_content_type = ContentType.objects.get_for_model(sender)
+        comment_content_type = ContentType.objects.get_for_model(Comment)
+        checkin_content_type = ContentType.objects.get_for_model(CheckIn)
         if instance.content_type is comment_content_type:
             send_comment_reply_notification.subtask((instance.id,)).apply_async()
+        elif instance.content_type is checkin_content_type:
+            send_checkin_reply_notification.subtask((instance.id,)).apply_async()
