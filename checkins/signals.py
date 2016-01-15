2369c54b8920ae347a6b0adbb3835492802837aa
mcmahonnate <mcmahon.nate@gmail.com>
2016-01-15 12:49:42 -0500

    Close out outstanding Check-In requests

--- checkins/signals.py
+++ checkins/signals.py
@@ -1,15 +1,27 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from checkins.models import CheckIn
+from checkins.models import CheckIn, CheckInRequest
 from tasks import *
 
 
 @receiver(post_save, sender=CheckIn)
 def checkin_save_handler(sender, instance, created, update_fields, **kwargs):
     if created:
-        send_checkin_request_notification.subtask((instance.id,)).apply_async()
+        # Close any outstanding Check-in requests
+        requests = CheckInRequest.objects.filter(requester=instance.employee, host=instance.host)
+        requests.update(was_responded_to=True)
+        request = requests.order_by('date').first()
+        if request is not None:
+            instance.checkin_request = request
+            instance.save()
     elif update_fields:
         if 'published' in update_fields and instance.published:
             send_employee_shared_checkin_notification.subtask((instance.id,)).apply_async()
         elif 'visible_to_employee' in update_fields and instance.visible_to_employee:
             send_host_sent_employee_checkin_notification.subtask((instance.id,)).apply_async()
+
+
+@receiver(post_save, sender=CheckInRequest)
+def checkin_save_handler(sender, instance, created, update_fields, **kwargs):
+    if created:
+        send_checkin_request_notification.subtask((instance.id,)).apply_async()
