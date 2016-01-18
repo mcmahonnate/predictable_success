8f6fa9175f20890decc223bf6d0ab67cc629cacb
mcmahonnate <mcmahon.nate@gmail.com>
2016-01-18 10:24:06 -0500

    Fix template for Request Check-In notification

--- checkins/tasks.py
+++ checkins/tasks.py
@@ -51,23 +51,25 @@ def send_employee_shared_checkin_notification(checkin_id):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
+
 @app.task
-def send_checkin_request_notification(checkin_id):
-    from checkins.models import CheckIn
-    checkin = CheckIn.objects.get(id=checkin_id)
-    recipient_email = checkin.host.email
+def send_checkin_request_notification(checkin_request_id):
+    from checkins.models import CheckInRequest
+    checkin_request = CheckInRequest.objects.get(id=checkin_request_id)
+    recipient_email = checkin_request.host.email
     if not recipient_email:
         return
     tenant = Customer.objects.filter(schema_name=connection.schema_name).first()
-    response_url = 'https://%s/#/checkins/%d' % (tenant.domain_url, checkin.id)
+    response_url = 'https://%s/#/checkin/%d' % (tenant.domain_url, checkin_request.requester.id)
     context = {
-        'recipient_first_name': checkin.host.first_name,
-        'employee_full_name': checkin.employee.full_name,
+        'recipient_first_name': checkin_request.host.first_name,
+        'employee_first_name': checkin_request.requester.first_name,
+        'employee_full_name': checkin_request.requester.full_name,
         'response_url': response_url,
     }
-    subject = "%s shared their notes with leadership" % checkin.employee.full_name
-    text_content = render_to_string('checkins/email/checkin_share_notification.txt', context)
-    html_content = render_to_string('checkins/email/checkin_share_notification.html', context)
+    subject = "%s would like you to check in with them" % checkin_request.requester.full_name
+    text_content = render_to_string('checkins/email/checkin_request_notification.txt', context)
+    html_content = render_to_string('checkins/email/checkin_request_notification.html', context)
     msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [recipient_email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
