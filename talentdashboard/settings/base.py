6d089dcc03fc2119ac455b8b5157d39299a6fa9a
mcmahonnate <mcmahon.nate@gmail.com>
2016-03-14 09:37:55 -0400

    Revert "Debugging new SMTP provider"
    This reverts commit 2230b169301f310cdb0ea3191fc62eb3c0e51830.

--- talentdashboard/settings/base.py
+++ talentdashboard/settings/base.py
@@ -41,7 +41,7 @@ REQUIRED_GROUPS = (
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Email settings
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ.get("EMAIL_HOST", '')
 EMAIL_PORT = os.environ.get("EMAIL_PORT", '')
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS", '')
