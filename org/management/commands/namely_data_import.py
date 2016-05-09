4393c12007a52f2d842bd8fea87650e931dbffa1
mcmahonnate <mcmahon.nate@gmail.com>
2016-05-09 11:19:16 -0400

    namely: whoops

--- org/management/commands/namely_data_import.py
+++ org/management/commands/namely_data_import.py
@@ -49,7 +49,7 @@ class Command(BaseCommand):
         headers = {'Authorization': 'Bearer %s' % tenant.namely_api_token}
         response_code = None
         keep_alive = True
-        last_record = '75fa5df9-11ed-4065-b16e-d5a4afbefbc5'
+        last_record = None
         while keep_alive and (response_code is None or response_code == 200):
             url = "%s&after=%s" % (api_url, last_record)
             print url
