18b75928440a8bcc6b96b2ce7d7793ccc814ed0a
mcmahonnate <mcmahon.nate@gmail.com>
2016-05-09 15:16:36 -0400

    namely: Add new employees, update departure date

--- org/management/commands/namely_data_import.py
+++ org/management/commands/namely_data_import.py
@@ -2,7 +2,6 @@ from assessment.models import AssessmentCategory, EmployeeAssessment, MBTI
 from comp.models import CompensationSummary
 from customers.models import Customer
 from datetime import datetime
-from decimal import Decimal
 from django.core.management.base import BaseCommand
 from django.db import connection
 from optparse import make_option
@@ -37,7 +36,7 @@ class Command(BaseCommand):
                 assessment.save()
                 print "Updated %s's %s" % (employee.full_name, assessment.category.name)
             return
-
+        active_ids = []
         limit = 25
         tenant = Customer.objects.filter(schema_name=connection.schema_name).first()
         if tenant.is_public_tenant() or \
@@ -45,13 +44,13 @@ class Command(BaseCommand):
                 tenant.namely_api_token is None:
             return
         # Get namely feed
-        api_url = "https://%s/profiles.json?filter[user_status]=active&sort=first_name&limit=%s" % (tenant.namely_api_url, limit)
+        profiles_api_url = "https://%s/profiles.json?filter[user_status]=active&sort=first_name&limit=%s" % (tenant.namely_api_url, limit)
         headers = {'Authorization': 'Bearer %s' % tenant.namely_api_token}
         response_code = None
         keep_alive = True
         last_record = None
         while keep_alive and (response_code is None or response_code == 200):
-            url = "%s&after=%s" % (api_url, last_record)
+            url = "%s&after=%s" % (profiles_api_url, last_record)
             print url
             response = requests.get(url, headers=headers)
             json = response.json()
@@ -59,7 +58,14 @@ class Command(BaseCommand):
             if len(json['profiles']) > 0:
                 for profile in json['profiles']:
                     namely_id = profile['id']
+                    active_ids.append(namely_id)
+
                     email = profile['email']
+                    preferred_name = profile['preferred_name']
+                    first_name = profile['first_name']
+                    last_name = profile['last_name']
+                    start_date = profile['start_date']
+                    departure_date = profile['departure_date']
                     reports_to_id = profile['reports_to'][0]['id'] if len(profile['reports_to']) > 0 else None
                     gender = profile['gender'][0] if profile['gender'] else None
                     salary_yearly_amount = profile['salary']['yearly_amount'] if profile['salary'] else None
@@ -78,9 +84,18 @@ class Command(BaseCommand):
                         lead = None
 
                     try:
-                        employee = Employee.objects.get(namely_id=namely_id, departure_date__isnull=True)
+                        employee = Employee.objects.get(namely_id=namely_id)
                     except Employee.DoesNotExist:
-                        employee = None
+                        try:
+                            employee = Employee.objects.get(email=email)
+                        except Employee.DoesNotExist:
+                            employee = Employee(first_name=first_name,
+                                                last_name=last_name,
+                                                display=True,
+                                                namely_id=namely_id,
+                                                email=email)
+                            employee.save()
+                            print "Added new employee %s" % employee.full_name
 
                     if employee:
                         print employee
@@ -89,6 +104,21 @@ class Command(BaseCommand):
                                 employee.leader = lead
                                 employee.save()
                                 print "Updated %s's manager to %s" % (employee.full_name, lead.full_name)
+                        if start_date:
+                            employee.hire_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+                        if employee.departure_date and departure_date is None:
+                            employee.departure_date = 'None'
+                            employee.save()
+                            print "Welcoming back %s" % employee.full_name
+                        if preferred_name and preferred_name != employee.first_name:
+                            print "Updating %s's first name to %s" % (employee.full_name, preferred_name)
+                            employee.first_name = preferred_name
+                            employee.save()
+                        if last_name and last_name != employee.last_name:
+                            print "Updating %s's last name to %s" % (employee.full_name, last_name)
+                            employee.last_name = last_name
+                            employee.save()
+
                         if email and (employee.email is None or employee.email != email):
                             employee.email = email
                             employee.save()
@@ -132,4 +162,20 @@ class Command(BaseCommand):
                     last_record = namely_id
             else:
                 keep_alive = False
+
+        # Update any employee departures
+        departed_employees = Employee.objects.filter(departure_date__isnull=True, namely_id__isnull=False).exclude(namely_id__in=active_ids)
+        for departed_employee in departed_employees:
+            profile_api_url = "https://%s/profiles/%s.json" % (tenant.namely_api_url, departed_employee.namely_id)
+            response = requests.get(profile_api_url, headers=headers)
+            if response.status_code == 200:
+                json = response.json()
+                if len(json['profiles']) > 0:
+                    profile = json['profiles'][0]
+                    departure_date = profile['departure_date']
+                    if departure_date:
+                        departure_date = datetime.strptime(departure_date, '%Y-%m-%d').date()
+                        departed_employee.departure_date = departure_date
+                        departed_employee.save()
+                        print "Updated %s's departure date to %s" % (departed_employee.full_name, departed_employee.departure_date)
         return
