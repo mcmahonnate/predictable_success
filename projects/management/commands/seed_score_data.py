24c27817c6bf930b09dc66fc1fa7d02495154e24
Ann Millspaugh <ann.millspaugh@gmail.com>
2016-05-23 14:35:27 -0700

    mgmt cmmd to seed jeff data

---
+++ projects/management/commands/seed_score_data.py
@@ -0,0 +1,48 @@
+import csv
+import StringIO
+import os
+from django.core.management.base import BaseCommand
+from os.path import dirname, realpath
+from projects.models import Project,PrioritizationRule, ScoringCategory, ScoringCriteria, ScoringOption
+import datetime
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        current_dir = os.path.dirname(realpath(__file__))
+        file_path = current_dir + '/criteria.csv'
+
+        ScoringOption.objects.all().delete()
+        ScoringCriteria.objects.all().delete()
+        ScoringCategory.objects.all().delete()
+
+        with open(file_path, 'rU') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name = row[0]
+                print 'NAME'
+                print name
+                category, created = ScoringCategory.objects.get_or_create(
+                    name=row[0]
+                    )
+
+                print 'CATEGORY!!'
+                print category
+
+                criteria, created = ScoringCriteria.objects.get_or_create(
+                    name=row[1],
+                    description=row[1],
+                    category=category
+                )
+
+                option, created = ScoringOption.objects.get_or_create(
+                    criteria=criteria,
+                    description=row[2],
+                    value=int(row[3])
+                )
+
+        scoring_categories = ScoringCategory.objects.all()
+        date = datetime.datetime.now()
+        rules = PrioritizationRule.objects.create(description='Rules as of' + str(date),
+                                       categories=scoring_categories)
+        rules.save()
