e64ceeb40a15b2f91e672fd888f77c79759030bc
mcmahonnate <mcmahon.nate@gmail.com>
2016-07-06 16:41:31 -0400

    checkins: don't show your own republish era checkins

--- checkins/models.py
+++ checkins/models.py
@@ -113,7 +113,8 @@ class CheckIn(models.Model):
                     user.employee.id == self.host.id:
                 return self.summary
         else:
-            if user.has_perm('checkins.view_checkin_summary'):
+            if user.has_perm('checkins.view_checkin_summary') and \
+                    user.employee.id != self.employee.id:
                 return self.summary
         return None
 
