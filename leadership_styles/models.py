e0c52e4d0dd0b189a8c7a352eb5b9255930928ba
mcmahonnate <mcmahon.nate@gmail.com>
2016-10-11 18:33:13 -0400

    whoops

--- leadership_styles/models.py
+++ leadership_styles/models.py
@@ -388,8 +388,9 @@ class EmployeeLeadershipStyle(models.Model):
     scores = models.ManyToManyField(Score, related_name='employee_leadership_style', null=True, blank=True)
 
     def _calculate_scores(self):
-        if self.all_questions_answered() and self.answers.count() > 0 and not self.completed:
+        if self.all_questions_answered() and self.answers.count() > 0:
             self.scores.clear()
+            self.scores.save()
             self.visionary_score = self.answers.filter(leadership_style=VISIONARY).count() * SCORE_MULTIPLIER
             self.scores.add(Score.objects.create_score(score=self.visionary_score, style=VISIONARY))
 
