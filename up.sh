563c37148436e602e70fc72325fdc4ac3f34599f
Adam Hill <adamghill@yahoo.com>
2016-10-12 11:52:30 -0400

    Update up.sh with more things for all the things.

--- up.sh
+++ up.sh
@@ -9,11 +9,9 @@ elif [ ! -z "$ENVIRONMENT" ]; then
     echo "ERROR: You almost ran up.sh on a non-dev environment"
     exit 1
 else
-    pip install -r requirements.txt;
-    python manage.py collectstatic --noinput --settings=predictable_success.settings.local;
+    pip install -r requirements.txt
+    python manage.py collectstatic --noinput --settings=predictable_success.settings.local
+    node_modules/.bin/gulp scripts
+    python manage.py migrate_schemas
     python manage.py runserver 0.0.0.0:8001
 fi
-
-
-
-
