d5d21f3996e7c1a7d54fcb9d0c3dee2b2d540c18
mcmahonnate <mcmahon.nate@gmail.com>
2016-10-26 15:23:30 -0400

    changing out email backend

--- predictable_success/settings/production.py
+++ predictable_success/settings/production.py
@@ -2,5 +2,5 @@
 from .base import *
 
 RAVEN_CONFIG['tags']['environment'] = 'production'
-EMAIL_BACKEND = 'predictable_success.backend.SSLEmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
