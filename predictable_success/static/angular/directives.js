ed9b64c4e540db45acb03d0d6a393227978a258b
mcmahonnate <mcmahon.nate@gmail.com>
2016-11-11 16:51:30 -0500

    Adding spinner to quiz

--- predictable_success/static/angular/directives.js
+++ predictable_success/static/angular/directives.js
@@ -471,6 +471,58 @@ angular.module('tdb.directives', ['ngTouch','ngAnimate'])
 	};
 }])
 
+.directive('spinner', ['$timeout', function($timeout) {
+    return {
+      restrict: 'E',
+      template: '<div class="spinner centered"><i class="spinner__dot spinner__dot--one"></i><i class="spinner__dot spinner__dot--two"></i><i class="spinner__dot spinner__dot--three"></i></div>',
+      scope: {
+        show: '=',
+        delay: '@'
+      },
+      link: function(scope, elem, attrs) {
+        var showTimer;
+
+        //This is where all the magic happens!
+        // Whenever the scope variable updates we simply
+        // show if it evaluates to 'true' and hide if 'false'
+        scope.$watch('show', function(newVal){
+          newVal ? showSpinner() : hideSpinner();
+        });
+
+        function showSpinner() {
+          //If showing is already in progress just wait
+          if (showTimer) return;
+
+          //Set up a timeout based on our configured delay to show
+          // the element (our spinner)
+          showTimer = $timeout(showElement.bind(this, true), getDelay());
+        }
+
+        function hideSpinner() {
+          //This is important. If the timer is in progress
+          // we need to cancel it to ensure everything stays
+          // in sync.
+          if (showTimer) {
+            $timeout.cancel(showTimer);
+          }
+
+          showTimer = null;
+
+          showElement(false);
+        }
+
+        function showElement(show) {
+          show ? elem.css({display:''}) : elem.css({display:'none'});
+        }
+
+        function getDelay() {
+          var delay = parseInt(scope.delay);
+
+          return angular.isNumber(delay) ? delay : 200;
+        }
+      }
+    };
+}])
 
 .directive('talentCategoryChart', ['$location', 'TalentCategories', function($location, TalentCategories) {
     return function(scope, element, attrs){
